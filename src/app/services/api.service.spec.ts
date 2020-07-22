8740c5717d498bb2b16785c6f9e9d4799b822628
Carlos Alejandro Hernández Mejía <alejandromejia2013.27@outlook.com>
2020-07-22 14:09:38 -0300

    Test Services

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,12 +1,93 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, getTestBed } from '@angular/core/testing';
 
 import { ApiService } from './api.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
+import { HttpHeaders } from '@angular/common/http';
 
-describe('ApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('ApiService', () => {
+  let service: ApiService;
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ApiService],
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.get(ApiService);
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+  });
 
-  it('should be created', () => {
-    const service: ApiService = TestBed.get(ApiService);
+  afterAll(() => {
+    injector = null;
+    service = null;
+    httpMock = null;
+  });
+
+  fit('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  fdescribe('GET', () => {
+    fit('should excute a GET petition', () => {
+      const result = 'testing';
+      service.get('/test').subscribe((resp) => {
+        expect(resp).toBe(result);
+      });
+      const req = httpMock.expectOne(`${environment.apiEndpoint}/test`);
+      expect(req.request.method).toBe('GET');
+      req.flush(result);
+    });
+
+    fit('should excute a GET petition with headers', () => {
+      const result = 'testing';
+      const headers = new HttpHeaders({test: 'prueba'});
+      service.get('/test', headers).subscribe((resp) => {
+        expect(resp).toBe(result);
+      });
+      const req = httpMock.expectOne(`${environment.apiEndpoint}/test`);
+      expect(req.request.method).toBe('GET');
+      expect(req.request.headers.get('test')).toBe('prueba');
+      req.flush(result);
+    });
+  });
+
+  fdescribe('POST', () => {
+    fit('should excute a POST petition', () => {
+      const result = 'testing';
+      service.post('/test', {}).subscribe((resp) => {
+        expect(resp).toBe(result);
+      });
+      const req = httpMock.expectOne(`${environment.apiEndpoint}/test`);
+      expect(req.request.method).toBe('POST');
+      expect(req.request.body).toEqual({});
+      req.flush(result);
+    });
+  });
+
+  fdescribe('PUT', () => {
+    fit('should excute a PUT petition', () => {
+      const result = 'testing';
+      service.put('/test', {}).subscribe((resp) => {
+        expect(resp).toBeTruthy();
+      });
+      const req = httpMock.expectOne(`${environment.apiEndpoint}/test`);
+      expect(req.request.method).toBe('PUT');
+      expect(req.request.body).toEqual({});
+      req.flush(result);
+    });
+  });
+
+  fdescribe('DELETE', () => {
+    fit('should excute a DELETE petition', () => {
+      const result = 'testing';
+      service.delete('/test').subscribe((resp) => {
+        expect(resp).toBeTruthy();
+      });
+      const req = httpMock.expectOne(`${environment.apiEndpoint}/test`);
+      expect(req.request.method).toBe('DELETE');
+      req.flush(result);
+    });
   });
 });
--- src/app/services/navigation.service.spec.ts
+++ src/app/services/navigation.service.spec.ts
@@ -1,12 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { NavigationService } from './navigation.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('NavigationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+fdescribe('NavigationService', () => {
+  let service: NavigationService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [NavigationService],
+      imports: [RouterTestingModule]
+    });
+    service = TestBed.get(NavigationService);
+  });
 
-  it('should be created', () => {
-    const service: NavigationService = TestBed.get(NavigationService);
+  fit('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  fit('should navigate go to pins', () => {
+    const navigate = spyOn((service as any).router, 'navigate');
+    service.goToPins();
+    expect(navigate).toHaveBeenCalledWith(['/app/pins']);
+  });
+
+  fit('should navigate go to editMode', () => {
+    const navigate = spyOn((service as any).router, 'navigate');
+    service.goToEditMode();
+    expect(navigate).toHaveBeenCalledWith(['/app/add']);
   });
 });
